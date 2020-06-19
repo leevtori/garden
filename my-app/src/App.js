a2f8be3b4280504744c491c53f761ea9706f4f44
leevtori <vlee2@ualberta.ca>
2020-06-19 16:36:46 -0600

    connected server + client

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+// import logo from './logo.svg';
 import './App.css';
+// import parse from '@mysql/xdevapi/lib/DevAPI/Util/URIParser';
+// import { response } from 'express';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error : null,
+      items: [] };
+  }
+  componentDidMount() {
+    this.fetchData();
+  }
+  fetchData() {
+      
+    fetch('/api/flower')
+      .then(res => res.text())
+      .then((body) => {
+        debugger;
+        this.setState({
+          items : JSON.parse(body)
+        });
+        console.log(JSON.parse(body));
+      },
+      (error) => {
+        this.setState({
+          error
+        });
+      }
+      )
+      .catch(error => console.log("parsing failed", error))
+  }
+
+  render() {
+    // const {error, items} = this.state;
+    const error = this.state.error;
+    // const items = this.state.items;
+    if (error){
+      return <div>Error: {error.message} </div>
+    } else {
+      return (
+        <div>
+          <h6> flower names </h6>
+          <ul>
+            {this.state.items[200][1]} hi
+
+
+            {/* {items[0]} */}
+            {/* {items.map(item => {
+              <li key={item[0]}>
+                {item[1]}
+              </li>
+            })} */}
+          </ul>
+  
+        </div>
+      )
+    }
+  }
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
--- my-app/src/routes.js
+++ my-app/src/routes.js
@@ -10,18 +10,26 @@ const config = {
     schema:     'garden'
 }
 
-app.use('/flowers', function (req, res, next) {
+app.get('/', function (req, res) {
+    res.send("hello world from express")
+})
+
+app.get('/api/flower', function (req, res, next) {
     mysql.getSession(config)
     .then(session => {
         return session.getSchema("garden").getTable("flower").select().execute()
     })
-    .then(result => console.log(result.fetchAll()))
+    .then(result => 
+        res.send(result.fetchAll())
+        // res.send(JSON.stringify(result.fetchAll()))
+        // res.json(JSON.stringify(result.fetchAll))
+    )
     .catch(function (err){
         console.log(err);
     })
 })
 
 
-app.listen(3000, () => {
-    console.log('Go to http://localhost:3000/flower so you can see the data.');
+app.listen(5000, () => {
+    console.log('Go to http://localhost:5000/flower so you can see the data.');
 });
